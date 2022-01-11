42287c45e365bc853ff9e4ed0275a711dc8b104d
Lauren Pandori <lauren_pandori@nps.gov>
2022-01-11 12:49:21 -0800

    silvetia data for sw - isolate site + yrs

---
+++ code/silvetia_data_iso_jan22.R
@@ -0,0 +1,24 @@
+#################################################
+# Title: Silvetia Data for SW
+# Purpose: isolate Silvetia cover data in Silvetia plots for SW
+# Author: LP
+# Created: 1/11/22
+# Last edited: 1/11/22
+##################################################
+
+##### packages #####
+library(readxl)
+library(janitor)
+library(tidyverse)
+
+##### load data #####
+setwd("D:/LP_Files/RStudio_Files/cabr_ri/data/MEDN_RI_DB_JAN21")
+target <- read_excel('Photoplot_summary_by_plot.xlsx')
+
+##### tidy #####
+target_sw <- target %>%
+  filter(SiteCode %in% c('CAB1', 'CAB2', 'CAB3', 'NAVYN', 'NAVYS') & 
+         SurveyYear %in% c(2019, 2020) & 
+         Zone == 'SIL' & 
+         SpeciesCode == 'SILCOM' &
+         SeasonName == 'Fall')
